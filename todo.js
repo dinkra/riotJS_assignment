3398e8f4864111975d2bd54962336c3820579532
dinkra <dinarochka@gmail.com>
2015-05-27 13:22:22 +0200

    Todo list

--- todo.js
+++ todo.js
@@ -8,11 +8,14 @@ var tasks = {
 
 var todoTag;
 
+
+//set array of tasks to LocalStorage
 function updateLocalStorage() {
 	var tasksArray = JSON.stringify(tasks.items);
 	localStorage.setItem('tasksArray', tasksArray);
 }
 
+//get array of tasks from LocalStorage
 function updateTasks() {
 	if (localStorage.length) {
 		tasks.items = JSON.parse(localStorage.getItem('tasksArray'));
@@ -20,7 +23,7 @@ function updateTasks() {
 	todoTag.update();
 }
 
-
+// add new task
 function addTask() {
 	var val = $("#newTask").val();
 	if (val) {
@@ -32,6 +35,7 @@ function addTask() {
 	return false;
 }
 
+//delete task
 function deleteTask(i) {
 	tasks.items.splice(i, 1);
 	todoTag.update();
@@ -39,6 +43,7 @@ function deleteTask(i) {
 
 }
 
+//done or undone task 
 function doneTask(i) {
 	if (tasks.items[i].done) {
 		tasks.items[i].done = false
@@ -48,8 +53,8 @@ function doneTask(i) {
 	updateLocalStorage();
 }
 
+//popup where is possible to change item
 function editTask(i) {
-
 	var x = window.prompt('Edit your task', tasks.items[i].title);
 	if (x) { 
 		tasks.items[i].title = x;
@@ -63,14 +68,17 @@ function editTask(i) {
 
 $(document).ready(function(){
 
+	//delete task event
 	$('ul').on('click', '.delete_task', function() {
         deleteTask($(this).attr('data-task-id'));
     });
 
+	//done task event
 	$('ul').on('click', '.done_task', function() {
         doneTask($(this).attr('data-task-id'));
     });
 
+	//edit task event
     $('ul').on('click', '.edit_task', function() {
         editTask($(this).attr('data-task-id'));
     });
